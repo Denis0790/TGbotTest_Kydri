047300823615ccdde57964ffe3596f49de2c2140
Denis0790 <134041882+Denis0790@users.noreply.github.com>
2023-11-12 21:30:31 +0300

    two

---
+++ TgBotKydri_test/app.py
@@ -0,0 +1,4 @@
+text_hyper_start1 = '<a href="https://t.me/mama_curls">#мамакудрей</a>'
+text_hyper_start2 = '<a href="https://t.me/mama_curls">МАМА КУДРЕЙ</a>'
+text_hyper_start3 = '<a href="https://t.me/kosmo_kudri">КОСМОКУДРИ</a>'
+
